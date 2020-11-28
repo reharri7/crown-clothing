b1e81863d772489b7bba27db5ca913be3c7ded9c
Rhett Harrison <reharri7@asu.edu>
2020-11-27 19:46:34 -0800

    removed compression

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -17,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
  
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression);
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
  
