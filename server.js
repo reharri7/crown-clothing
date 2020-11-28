9e276abbff74cdf826c31b7bf752f5f18bc8dec1
Rhett Harrison <reharri7@asu.edu>
2020-11-27 16:39:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
-
+ 
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
